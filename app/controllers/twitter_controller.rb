863fd65f1926f2ec019ff2c3700188be5056420b
Unai Trecet <ut@12auto.de>
2017-11-19 19:37:11 +0100

    Using index action as unique action.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,7 +1,19 @@
 class TwitterController < ApplicationController
+  before_action :set_twitter_client
 
-  def search
+  def index
+    @tweets = []
 
+    @client.search("to:#{ params[:search]}", result_type: "recent").take(10).each do |tweet|
+      @tweets << tweet
+    end
+
+  end
+
+  private
+
+  def set_twitter_client
+    @client = Twitter::REST::Client.new(Rails.configuration.twitter)
   end
 
 end
