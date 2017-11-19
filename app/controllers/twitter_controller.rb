3fcff0cf9da9a4eace2f7c1c884d939362275bc0
Unai Trecet <ut@12auto.de>
2017-11-19 21:42:33 +0100

    Some refactoring.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,18 +2,17 @@ class TwitterController < ApplicationController
   before_action :set_twitter_client
 
   def index
-    @tweets = []
-
-    @client.search("to:#{ params[:search]}", result_type: "recent").take(10).each do |tweet|
-      @tweets << tweet
-    end
-
+    @tweets = params[:search].present? ? search : []
   end
 
   private
 
   def set_twitter_client
     @client = Twitter::REST::Client.new(Rails.configuration.twitter)
+  end
+
+  def search
+    @client.search("to:#{ params[:search]}", result_type: "recent").take(10)
   end
 
 end
